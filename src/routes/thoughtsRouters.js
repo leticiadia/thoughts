01a15d9ff7d450ef3e339e12f85686c827331e6b
Leticia Dias <contatoleticiadia@gmail.com>
2023-11-29 11:47:41 -0300

    feat(routes): added routes to thoughts

---
+++ src/routes/thoughtsRouters.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const ThoughtsController = require("../controllers/ThoughtsController");
+
+const router = express.Router();
+
+router.get("/thoughts/dashboard", ThoughtsController.dashboard);
+router.get("/thought-create", ThoughtsController.registerThought);
+router.post("/thoughts/create", ThoughtsController.createThought);
+
+router.get("/thoughts/edit/:id", ThoughtsController.showPageEditThought);
+router.post("/thoughts/update/:id", ThoughtsController.updateThought);
+
+router.post("/thoughts/remove/:id", ThoughtsController.deleteThought);
+
+// router.get("/thoughts", ThoughtsController.findAllThoughts);
+router.get("/thoughts/:id", ThoughtsController.findThoughts);
+
+module.exports = router;
