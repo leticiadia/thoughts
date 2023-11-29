8934799110fbdd5124c121aadc94005a9a73201e
Leticia Dias <contatoleticiadia@gmail.com>
2023-11-29 12:07:37 -0300

    feat(controllers): showPageEditThought function created

--- src/controllers/ThoughtsController.js
+++ src/controllers/ThoughtsController.js
@@ -1,10 +1,20 @@
 const Thought = require("../model/Thought");
 
 module.exports = {
-  async findAllThoughts(request, response) {
+  async dashboard(request, response) {
     const thoughts = await Thought.findAll({ raw: true });
 
-    return response.json(thoughts);
+    return response.render("thoughts/dashboard", { thoughts });
+  },
+
+  // async findAllThoughts(request, response) {
+  //   const thoughts = await Thought.findAll({ raw: true });
+
+  //   return response.json(thoughts);
+  // },
+
+  async registerThought(request, response) {
+    return response.render("thoughts/register");
   },
 
   async createThought(request, response) {
@@ -12,7 +22,13 @@ module.exports = {
 
     const thoughts = await Thought.create({ title, description });
 
-    return response.json(thoughts);
+    try {
+      if (thoughts) {
+        return response.redirect("/thoughts/dashboard");
+      }
+    } catch (error) {
+      return console.error(error);
+    }
   },
 
   async findThoughts(request, response) {
@@ -21,6 +37,14 @@ module.exports = {
     const thought = await Thought.findOne({ where: { id: id } });
 
     return response.json(thought);
+  },
+
+  async showPageEditThought(request, response) {
+    const { id } = request.params;
+
+    const thought = await Thought.findOne({ where: { id: id }, raw: true });
+
+    return response.render("thoughts/edit", { thought });
   },
 
   async updateThought(request, response) {
@@ -32,7 +56,13 @@ module.exports = {
       { where: { id: id } }
     );
 
-    return response.json(thought);
+    try {
+      if (thought) {
+        return response.redirect("/thoughts/dashboard");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   },
 
   async deleteThought(request, response) {
@@ -40,8 +70,12 @@ module.exports = {
 
     const thought = await Thought.destroy({ where: { id: id } });
 
-    return response.json({
-      message: "Pensamento deletado com sucesso!",
-    });
+    try {
+      if (thought) {
+        return response.redirect("/thoughts/dashboard");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
