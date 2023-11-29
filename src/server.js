931b254ca623489b4f2ae073ec3a8712f90d9c19
Leticia Dias <contatoleticiadia@gmail.com>
2023-11-29 12:01:47 -0300

    feat(server): config template engine handlebars and routes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
 const express = require("express");
-const router = require("./routes");
+const exphbs = require("express-handlebars");
+// const Handlebars = require("handlebars");
+const path = require("path");
 
+// Importar as rotas
+const thoughtsRouters = require("./routes/thoughtsRouters");
+const usersRouters = require("./routes/usersRouters");
+
+// Import do arquivo do banco de dados
 require("./database/index");
 
+// Inicializando o express
 const app = express();
-app.use(express.json());
 
-app.use(router);
+//Import o template de engenharia Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.set("views", path.join(__dirname, "views/"));
+
+app.use(express.static(__dirname + "/public"));
+
+exphbs.create({
+  partialsDir: path.join(__dirname, "views/partials"),
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(thoughtsRouters);
+app.use(usersRouters);
 
 app.listen(3333, console.log("Server is running in port 3333"));
